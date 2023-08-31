4c15fcb5fbde7f4df08fcd3c3d60ed6230cfe3a5
User <pavadi.abhishek@gmail.com>
2023-08-30 21:19:07 -0700

    SMS Send using Twilio -Inittial Commit

---
+++ src/main/java/com/example/smssender/SmsService.java
@@ -0,0 +1,32 @@
+package com.example.smssender;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+
+
+@Component
+	public class SmsService {
+
+	    
+	    private final String ACCOUNT_SID ="ACe804b1f081fa9a00a714432bac1b855d";
+
+	    private final String AUTH_TOKEN = "eeb6f2ecc9910748444ec42534775767";
+
+	    private final String FROM_NUMBER = "+16185434617"; //Twilio number
+
+	    public void send(SmsPojo sms) {
+	    	Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+
+	        Message message = Message.creator(new PhoneNumber(sms.getTo()), new PhoneNumber(FROM_NUMBER), sms.getMessage())
+	                .create();
+	        System.out.println("here is my id:"+message.getSid());// Unique resource ID created to manage this transaction
+
+	    }
+
+	    public void receive(MultiValueMap<String, String> smscallback) {
+	    }
+	
+}
---
+++ src/main/java/com/example/smssender/SpringBootOtpAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.smssender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOtpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOtpAppApplication.class, args);
+	}
+
+}
